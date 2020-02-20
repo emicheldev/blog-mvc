8b63494fc3128205fff211c90f8fe01b9da49810
emichel <emicheldev@gmail.com>
2020-02-20 12:26:40 +0100

    Create HomeController.php

---
+++ src/Controller/HomeController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Controller;
+
+use App\Model\Factory\ModelFactory;
+use Twig\Error\LoaderError;
+use Twig\Error\RuntimeError;
+use Twig\Error\SyntaxError;
+
+/**
+ * Class HomeController
+ * Manages the Homepage
+ * @package App\Controller
+ */
+class HomeController extends MainController
+{
+
+}
