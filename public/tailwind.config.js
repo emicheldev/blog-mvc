c1c0e8a4e8563f0e72d527352b20b88783e06fad
emichel <emicheldev@gmail.com>
2020-02-25 14:30:13 +0100

    installation of tailwind css and laravel-mix

---
+++ public/tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  theme: {
+    fontSize: {
+      '7xl': '15rem',
+      'year':'8rem;'
+  },
+  fontFamily: {
+    body: ['Nunito', 'sans-serif'],
+  },
+    extend: {},
+  },
+  variants: {},
+  plugins: [],
+}
